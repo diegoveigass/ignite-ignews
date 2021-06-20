f5d774a8ade4f87bedfb132c3ad581eb2669bad7
diegoveigass <sdiegoveiga@gmail.com>
2021-06-20 13:20:35 -0300

    Consumindo API do Stripe (SSR)
    fix: revert

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home({ product }: HomeProps) {
           <SubscribeButton priceId={product.priceId} />
         </section>
 
-        <img src="/images/avatar.svg" alt="Girl coding" />
+        <img src="/images/avatar.svg" alt="Girl codings" />
       </main>
     </>
   );
